f3814fb0aa621fea9aa40086fa67d12a162d62f7
watergun <jiashui@hotmail.com>
2016-09-04 21:59:09 -0500

    Update 9-7.c

--- 9-7.c
+++ 9-7.c
@@ -1,4 +1,4 @@
-#include<stdio.h>/*实现不了。等待修改。。。
+#include<stdio.h>
 void power(double a,int b);
 double multi(double a,int b);
 int main()
@@ -14,26 +14,28 @@ int main()
 }
 void power(double a,int b)
 {
-    double po;
+    double pow;
     if(a==0)
-        po=0;
+        pow=0;
     else if(b==0)
-        po=1;
-    else if(b<0)
+        pow=1;
+    else if (b<0)
         {
             b=-b;
-            po=multi(a,b);
-            po=(1/po);
+            pow=multi(a,b);
+            pow=(1/pow);
+            b=-b;
         }
     else
-        po=multi(a,b);
-    printf("%.2lf ^%d = %2lf\n",a,b,po);
+        pow=multi(a,b);
+    printf("%.3lf ^%d = %.5lf\n",a,b,pow);
 }
 
 double multi(double a,int b)
 {
-    int t,po;
-    for(po=1;t<b;t++)
+    int t=0;
+    double po=1;
+    for (po=1;t<b;t++)
         po=po*a;
     return po;
 }
