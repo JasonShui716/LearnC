f591b484540c8d79f6164298bc00e31b11dea59b
watergun <jiashui@hotmail.com>
2016-10-13 16:34:43 +0800

    Update 10-8.c

--- 10-8.c
+++ 10-8.c
@@ -13,13 +13,13 @@ void cnp(int n,int m,double arr1[n][m],double arr2[n][m])
   int ic,ir;
   for(ic=0;ic<n;ic++)
   {
-   	for(ir=0;ir<m;ir++)
+      for(ir=0;ir<m;ir++)
       {
       	**arr2=**arr1;
         printf("[%d][%d]%.1lf ",ic,ir,arr2[ic][ir]);
         arr1 = *(arr1+ic)+1;
         arr2 = *(arr2+ic)+1;
-			}
+      }
       printf("\n");
   }
 }           
