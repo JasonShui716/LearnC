7e5816de55506582a662dd6fba2afe92abc38ee3
watergun <jiashui@hotmail.com>
2016-10-13 16:34:01 +0800

    Update 10-8.c

--- 10-8.c
+++ 10-8.c
@@ -2,15 +2,15 @@
 void cnp(int n,int m,double arr1[n][m],double arr2[n][m]);
 int main()
 {
-	double array1[3][5]={{1,3,5,7,9},{2,2,3,4,5},{3,2,5,6,5}};
-	double array2[3][5];
-	cnp(3,5,array1,array2);//cnp=copy'n'print
+  double array1[3][5]={{1,3,5,7,9},{2,2,3,4,5},{3,2,5,6,5}};
+  double array2[3][5];
+  cnp(3,5,array1,array2);//cnp=copy'n'print
   printf("test%.1f",array2[2][4]);
   return 0;
 } 
 void cnp(int n,int m,double arr1[n][m],double arr2[n][m])
 {
-	int ic,ir;
+  int ic,ir;
   for(ic=0;ic<n;ic++)
   {
    	for(ir=0;ir<m;ir++)
