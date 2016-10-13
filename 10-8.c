98a35b4cf919c74e0d5d489893151c16726c0dec
watergun <jiashui@hotmail.com>
2016-10-13 16:21:01 +0800

    Update 10-8.c

--- 10-8.c
+++ 10-8.c
@@ -1,16 +1,13 @@
-/*#include<stdio.h>
+#include<stdio.h>
 void cnp(int n,int m,double arr1[n][m],double arr2[n][m]);
 int main()
 {
-	int a=3;
-	int b=5;
 	double array1[3][5]={{1,3,5,7,9},{2,2,3,4,5},{3,2,5,6,5}};
 	double array2[3][5];
-	cnp(a,b,array1,array2);//cnp=copy'n'print
+	cnp(3,5,array1,array2);//cnp=copy'n'print
 	printf("test%.1f",array2[2][4]);
 	return 0;
 } 
-
 void cnp(int n,int m,double arr1[n][m],double arr2[n][m])
 {
 	int ic,ir;
@@ -20,9 +17,9 @@ void cnp(int n,int m,double arr1[n][m],double arr2[n][m])
 					{
 						**arr2=**arr1;
 						printf("[%d][%d]%.1lf ",ic,ir,arr2[ic][ir]);
-						arr1=*(arr1+ic)+1;
-						arr2=*(arr2+ic)+1;
+						arr1 = *(arr1+ic)+1;
+						arr2 = *(arr2+ic)+1;
 					}
 					printf("\n");
-			}
-	}			
+		}
+}			
