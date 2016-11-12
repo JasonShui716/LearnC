5002d4705e7a4167d3e870b24a67e0c137e5eb6c
watergun <jiashui@hotmail.com>
2016-11-12 19:03:15 +0800

    Add files via upload

---
+++ 13-2v1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 100
+int main()
+{
+  FILE *fi,*fo;
+  int ch;
+  char finame[40];
+  char foname[40];
+  char line[MAX];
+  printf("Please enter the input file name:");
+  gets(finame);
+  if((fi=fopen(finame,"r"))==NULL)
+  {
+    fprintf(stderr,"can't open the file %s.",finame);
+    exit(0);
+  }
+  printf("Please enter the output file name:");
+  gets(foname);
+  fo=fopen(foname,"w");
+  while((ch=getc(fi))!=EOF)
+  {
+    ungetc(ch,fi);
+    fgets(line,MAX,fi);
+    fputs(line,fo);
+  }
+  fclose(fi);
+  fclose(fo);
+}
