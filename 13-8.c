9466d668007925f2ae97316526085200dcb9c9ae
watergun <jiashui@hotmail.com>
2016-11-27 16:11:38 +0800

    Add files via upload

---
+++ 13-8.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+#define MAX 100 
+int main()
+{
+    char ch,fch;
+    char files[MAX];
+    char cache[MAX];
+    int n=0;
+    printf("Please enter an character:");
+    scanf("%c",&ch);
+    getchar();
+    printf("Please enter the files you want to search in, multiple files should be separated by spaces:");
+    fgets(files,MAX,stdin);
+    /*chai fen wen jian ming*/
+    /*loop getc(),find all the char*/
+    /*next file*/
+}
+    
